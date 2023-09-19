f4b8cdcc5236621c2999708016893979ddc5fc2e
miaguirre22 <aguirremartinignacio@gmail.com>
2023-09-19 15:32:00 -0300

    book store page

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://miaguirre22:NUOB5d2oUyDoM10X@cluster0.frirr.mongodb.net/books-collection?retryWrites=true&w=majority'
---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import ShowBook from './pages/ShowBook';
+import CreateBooks from './pages/CreateBooks';
+import EditBook from './pages/EditBook';
+import DeleteBook from './pages/DeleteBook';
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='/books/create' element={<CreateBooks />}></Route>
+      <Route path='/books/details/:id' element={<ShowBook />}></Route>
+      <Route path='/books/edit/:id' element={<EditBook />}></Route>
+      <Route path='/books/delete/:id' element={<DeleteBook />}></Route>
+    </Routes>
+  )
+}
